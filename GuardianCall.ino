6ac9a6b2b746d4b87f3dafc24cc9e5849bc51336
Endeavor360 <kirangunathilaka@gmail.com>
2024-05-25 03:02:34 +0530

    Second Commit

--- GuardianCall.ino
+++ GuardianCall.ino
@@ -1,13 +1,23 @@
 #include "WiFi.h"
 #include "time.h"
 #include "Preferences.h"
+#include <esp_now.h>
 
 #include <Firebase_ESP_Client.h>
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 
+#include "SPI.h"
+#include <TFT_eSPI.h> // Hardware-specific library
+
+TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+
 #define API_KEY "Firebase_API_key"
 #define DATABASE_URL "FireBase_database_link"
+
+// here pins are assigned for SPI2 interface of ESP32 S3 Devkit 
+
+String DeviceID = "gc100001";
 
 FirebaseData fbdo;
 FirebaseAuth auth;
@@ -30,9 +40,49 @@ const char* ntpServer = "in.pool.ntp.org";
 const long  gmtOffset_sec = 19800;//GMT+5:30
 const int   daylightOffset_sec = 0;
 
+struct __attribute__((packed)) dataPacket { 
+  int btnID;
+  bool btnState;
+};
+
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  
+  dataPacket packet;
+  memcpy(&packet, incomingData, sizeof(packet));
+  int btnID = (int)packet.btnID;
+  bool btnState = (bool)packet.btnState;
+  Serial.println(btnID);
+  Serial.println(btnState, true);
+}
+
 void setup(){
   Serial.begin(115200);
+
+  tft.init();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextSize(1);
+  tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+
+  targetTime = millis() + 1000;
+
+
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
+  
+
+
   preferences.begin("Settings" , false);//opens a preferences library with the namespace "Settings"
+  tft.begin();
+
 
   String ssid = preferences.getString(ssidMem, "");  //search for stored ssid and pwd in the preferences and return "" found none
   String password = preferences.getString(passwordMem  , "");
@@ -73,27 +123,16 @@ void setup(){
   
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
+  
 }
 
 
 
 void loop(){
-  delay(1000);
-  printLocalTime();
 
   if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > sendDataIntervalMillis || sendDataPrevMillis == 0)){
     sendDataPrevMillis = millis();
 
-    int randNumber = random(15, 40);
-    float f = (float)randNumber / 1.01;
-    int i = (int(f*100));
-    store_random_Float_Val = float(i) / 100;
-    store_random_Int_Val = random(10, 99);
-
-    Serial.print("Random Float_Val : ");
-    Serial.println(store_random_Float_Val);
-    Serial.print("Random Int_Val   : ");
-    Serial.println(store_random_Int_Val);
     
     // Write an Int number on the database path test/random_Float_Val.
     if (Firebase.RTDB.setFloat(&fbdo, "Device1/random_Float_Val", store_random_Float_Val)) {
@@ -116,6 +155,8 @@ void loop(){
       Serial.println("FAILED");
       Serial.println("REASON: " + fbdo.errorReason());
     }
+
+    printLocalTime();
   }
 }
 
@@ -128,22 +169,28 @@ void printLocalTime(){
     Serial.println("Failed to obtain time");
     return;
   }
-  Serial.println(&timeinfo, "%A, %B %d  %H:%M");
+
+  tft.println(&timeinfo, "%A, %B %d ");
+  tft.println(&timeinfo, "%H:%M");
 }
 
 
 
 bool initialConnection(){
-  Serial.println("Nearby Networks");
+  tft.fillScreen(ILI9341_BLACK);
+  tft.setCursor(0, 0);
+  
+  //remove when touch available
+  tft.println("Enter the index of the wifi network you want to connect");
+  tft.println("Nearby Networks");
+
   byte numNetworks = WiFi.scanNetworks(); //scan for wifi networks and return number of networks found
 
   for (int i = 0; i < numNetworks; ++i) {
-    Serial.print(i+1);
-    Serial.print(")  ");
-    Serial.println(WiFi.SSID(i)); //Iterate over WiFi object (global arduino core object) i times 
+    tft.print(i+1);
+    tft.print(")  ");
+    tft.println(WiFi.SSID(i)); //Iterate over WiFi object (global arduino core object) i times 
   }
-
-  Serial.println("Enter the index of the wifi network you want to connect");
 
   while(!Serial.available()){
     //this loop will halt until serail input is given
@@ -155,8 +202,10 @@ bool initialConnection(){
   if(intInp < numNetworks+1){
     String inpSSID = WiFi.SSID(intInp-1);
 
-    Serial.print("Enter password of ");
-    Serial.println(inpSSID);
+    tft.fillScreen(ILI9341_BLACK);
+    tft.setCursor(0, 0);
+    tft.print("Enter password of ");
+    tft.println(inpSSID);
 
     while(!Serial.available()){
     //this loop will halt until password is given via serial
@@ -172,29 +221,33 @@ bool initialConnection(){
 
 bool connectWifi(String id,String  pwd){
   // Connect to Wi-Fi
-  Serial.print("Connecting to ");
-  Serial.println(id);
+  tft.fillScreen(ILI9341_BLACK);
+  tft.setCursor(0, 0);
+  tft.print("Connecting to ");
+  tft.println(id);
   WiFi.begin(id.c_str(), pwd.c_str());
 
   byte attempts = 0;
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     attempts++;
-    Serial.print(".");
+    tft.print(".");
     if (attempts > 20){
-      Serial.print("Can't connect to ");
-      Serial.print(id);
-      Serial.println(". Check your credentials");
+      tft.print("Can't connect to ");
+      tft.print(id);
+      tft.println(". Check your credentials");
+      delay(2000);
       WiFi.disconnect(true);
       return false;
     }
   }
 
   if (WiFi.status() == WL_CONNECTED){
-    Serial.println("WiFi connected.");
+    tft.println("WiFi connected.");
 
     preferences.putString(ssidMem, id.c_str() ); //stores the successfully conneted wifi credentials on the preferences namespace named  settings
     preferences.putString(passwordMem, pwd.c_str() );
     return true;
   }
 }
+
